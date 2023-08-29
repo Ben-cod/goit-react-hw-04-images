3f9c2a13e8f4df2532f15bd385c97a9bacee13d2
Ben-cod <123839625+Ben-cod@users.noreply.github.com>
2023-08-29 15:51:56 +0300

    add

--- src/components/Modal/modal.js
+++ src/components/Modal/modal.js
@@ -3,20 +3,19 @@ import css from './Modal.module.css';
 
 export const Modal = ({ onModalClose, largeImageURL, tag }) => {
   useEffect(() => {
-    const handleKeyDown = e => {
-      if (e.keyCode === 27 || e.currentTarget === e.target) {
-        onModalClose();
-      }
-    };
-
     window.addEventListener('keydown', handleKeyDown);
 
     return () => {
       window.removeEventListener('keydown', handleKeyDown);
     };
   }, [onModalClose]);
+  const handleKeyDown = e => {
+    if (e.keyCode === 27 || e.currentTarget === e.target) {
+      onModalClose();
+    }
+  };
   return (
-    <div className={css.Overlay} onClick={onModalClose}>
+    <div className={css.Overlay} onClick={handleKeyDown}>
       <div className={css.Modal}>
         <img src={largeImageURL} alt={tag} />
       </div>
